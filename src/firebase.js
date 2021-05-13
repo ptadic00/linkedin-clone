d4833ddf0998285956b6993e75989223a515a17e
ptadic00 <ptadic00@fesb.hr>
2021-05-13 09:26:00 +0200

    first commit: frontend and starting using redux

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDe42Cc9K0JJ0yfrGgVeoOfl-Q2LoSIg-E",
-  authDomain: "linkedin-clone-b9283.firebaseapp.com",
-  projectId: "linkedin-clone-b9283",
-  storageBucket: "linkedin-clone-b9283.appspot.com",
-  messagingSenderId: "31170734435",
-  appId: "1:31170734435:web:5ec8865d6f69e44a11cb56",
+  /*secret*/
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
